e7f3d89a25ec02f3266c8c92512399516077f773
Omo-Mummy <olayinkaoyedele9@gmail.com>
2023-02-16 15:50:49 -0800

    Eight

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main -  A C program that prints a line to the standard error
- */Return 1 (Success)
+ *Return: 1 (Success)
 */
 int main(void)
 {
