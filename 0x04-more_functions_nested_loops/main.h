143f0707e42e4dabe6d66f964fea280640a155a4
Omo-Mummy <olayinkaoyedele9@gmail.com>
2023-02-23 13:26:44 -0800

    Task-0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File - main.h
+ * Desc: Header contains protypes for all functions
+ * used in 0x03-more_functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+
+
+
+
+#endif
