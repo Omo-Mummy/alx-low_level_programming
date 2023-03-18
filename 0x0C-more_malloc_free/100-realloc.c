fa38f5eaa1cb533ea730f6c3de3d2855d7f47810
Omo-Mummy <olayinkaoyedele9@gmail.com>
2023-03-17 17:53:56 -0700

    Five

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,27 +1,50 @@
+#include <stdlib.h>
 #include "main.h"
-
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ **_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
+ *
+ * Return: pointer to the newly allocated memory block
  */
-
-int main(int argc, char *argv[])
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned long mul;
-	int i, j;
-	{printf("Error\n");
-		exit(98);
-		for (i = 1; i < argc; i++)
-		{
-			for (j = 0; argv[i][j] < 48;)
-			{printf("Error\n");
-				exit(98);
-			}
-		}
-		mul = atoi (argv[1] * atoi(argv[2]);
-		printf("%lu\n", mul);
-	return (0);
+
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
 	}
+
+	if (!ptr)
+		return (malloc(new_size));
+	ptr1 = malloc(new_size);
+
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+
+			ptr1[i] = old_ptr[i];
+	}
+	free(ptr);
+	return (ptr1);
 }
