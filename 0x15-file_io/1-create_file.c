f9ae28b2f2a07f4b08689206e63eeccaa050b758
Omo-Mummy <olayinkaoyedele9@gmail.com>
2023-04-09 16:48:16 -0700

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,8 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (numletters = 0; text_content[numletters]; numletters++);
+	for (numletters = 0; text_content[numletters]; numletters++)
+		;
 
 	wr = write(fd, text_content, numletters);
 
